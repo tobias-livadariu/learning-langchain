dd9ea84099ccfef5c8d66775ccc04acb6327ce0b
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-08-26 01:23:05 -0400

    Used OpenAI text embeddings for youtube assitant

---
+++ youtube-assistant/langchain_helper.py
@@ -0,0 +1,25 @@
+from langchain_community.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+video_url = "https://www.youtube.com/watch?v=-Osca2Zax4Y"
+
+def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    docs = text_splitter.split_documents(transcript)
+
+    db = FAISS.from_documents(docs, embeddings)
+    return db
+
+print(create_vector_db_from_youtube_url(video_url))
